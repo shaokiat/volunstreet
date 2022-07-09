9dcfdbb86503f6b20810afde21e82ff4d8d69911
shaokiat <shaokiat@gmail.com>
2022-07-09 13:56:22 +0800

    add prettier eslint and title

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Head>
+        <title>Volunstreet</title>
+        <meta name="description" content="Volunstreet App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
   )
 }
